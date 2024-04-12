0f52061692974705b5d4c4f5a4c6500771ec3383
Blandine NAHORNY <blandine.nahorny@edu.univ-fcomte.fr>
2024-04-12 13:56:31 +0200

    feat : HTML du menu

---
+++ src/pages/accordeon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const maVar = ref(true)
+const maVar2 = ref(1)
+</script>
+
+<template>
+<p>Page Accordéon</p>
+<p>la variable vaut : {{ maVar }}</p>
+
+<button @click="maVar = false">Cacher</button>
+
+<button @click="maVar = true">Montrer</button>
+
+<button @click="maVar = !maVar">Bascule</button>
+
+<button @click="maVar == 1" class="text-2xl"> Montrer 1 </button>
+<h1 v-show="maVar == 2" class="text-2xl"> Contenu 1 </h1>
+
+<button click@="maVar2 = false ? 0 : 1 "> </button>
+
+  <h1 v-show="maVar2" class="text-2xl">Bonjour tout le monde !</h1>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue'
+const maVar = ref(true)
+const maVar2 = ref(1)
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+<p>Page Accueil</p>
 </template>
